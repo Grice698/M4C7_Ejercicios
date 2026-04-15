4a8c03e2f8b6756ddaa7d237c83b402b4a344757
Grice698 <ariari.useless@gmail.com>
2026-04-15 17:09:59 +0200

    Refactor operation logic into operacion function
    Removed initial attempt

--- M4C7_EJERCICIO_PRACTICO.js
+++ M4C7_EJERCICIO_PRACTICO.js
@@ -4,24 +4,6 @@ Si el número creado es mayor que 50, la consola registra "¡El número es mayor
 Si es más pequeño, la consola registra "¡El número es menor que 50!"
 */
 
-/*
-intento 1. Tengo dudas de si este planteamiento responde al ejercicio porque la operación como tal está establecida 
-al final y no como parte de la función pero por si acaso la pongo ya que da el resultado bien. De igual forma la considero importante 
-ya que es parte del proceso
-*/
-
-var a = 1;
-var b = 2;
-var c = 3;
-var d = 4;
-
-function operation(resultado){
-resultado > 50 ? console.log("¡El número es mayor que 50!") : console.log("¡El número es menor que 50!");
-}
-operation((a + b) * (c + d));
- 
-// intento 2 
-
 function operacion(a, b, c, d) {
   var resultado = (a + b) * (c + d);
   if (resultado > 50) {
